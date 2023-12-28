2eb9321f0c553aae9232dc679b6b6b62f3ffaec7
tom <tsadowski93@gmail.com>
2023-12-28 13:27:54 -0500

    first commit

---
+++ config.h
@@ -0,0 +1,29 @@
+// tsadowski
+
+#pragma once
+
+// The default is 50. This makes the mouse ~3 times faster and more accurate
+#define MOUSEKEY_INTERVAL 16
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+#define MOUSEKEY_TIME_TO_MAX 80
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+#define MOUSEKEY_DELAY 100
+// It makes sense to use the same delay for the mouseweel
+#define MOUSEKEY_WHEEL_DELAY 100
+// The default is 100
+#define MOUSEKEY_WHEEL_INTERVAL 50
+// The default is 40
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+
+// recognize primary/secondary halves
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2500
+
+// combo reference layer
+#define COMBO_ONLY_FROM_LAYER 0
+#define COMBO_SHOULD_TRIGGER
+// combo shapes should make it easy to hit keys at the same time,
+// a narrow activation window minimizes accidental triggering
+#define COMBO_STRICT_TIMER
+#define COMBO_TERM 38
---
+++ readme.md
@@ -0,0 +1,26 @@
+
+this keymap aims to minimize time-dependent behavior while making every key immediately accessible. it achieves this by using combos. 
+the following characters and functions are accessed exclusively through combos:
+
+alt
+ctrl
+gamelayer
+mouselayer
+tab
+shift
+capslock
+defaultlayer
+[ {
+] }
+esc
+enter
+osgui
+- _
+= +
+' "
+; :
+` ~
+printscreen
+
+using combos to this extent removes the need for one-shot mods, one-shot layers, and hold-tap functions.   
+see source code for more information.   
