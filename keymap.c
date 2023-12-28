827e38454d8250a0099b02a7fabef4fe2444ada0
tom <tsadowski93@gmail.com>
2023-12-28 15:30:38 -0500

    update game layer

--- keymap.c
+++ keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [GAME_LYR] = LAYOUT_split_3x5_2( // NO COMBOS OTHER THAN LAYER SWITCHING -- APPROPRIATE FOR GAMING
         KC_1, KC_2, KC_3, KC_4, KC_5,                      KC_6, KC_7, KC_8, KC_9, KC_0,
-        KC_A, KC_W, KC_S, KC_D, KC_Q,                      KC_E, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-        KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_E,            KC_Q, KC_A, KC_S, KC_W, KC_D,
+        KC_A, KC_W, KC_S, KC_D, KC_F,                      KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_Z, KC_X, KC_C, KC_V, KC_B,                      KC_N, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
                                              KC_ESC, KC_L,             KC_R, KC_ENT)
 };
 
